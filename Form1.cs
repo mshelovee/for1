de5646a899a3ed0c77052f993cf983d8623a731d
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-23 12:29:00 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_1__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+  
+            for(int i = 0; i < 10; i++) //"i++" birer birer arttırmak.
+            {
+                string metin = i.ToString() + " - " + ad;
+                lbAd.Items.Add(metin);
+
+            }
+        }
+    }
+}
